a1c880facc1c920f3a85f1214acf962de83ed311
Евгений Червев <evgeniy_cheraev@vk.com>
2025-05-17 00:22:53 +0300

    update

--- src/discord-bot/discord-bot.service.ts
+++ src/discord-bot/discord-bot.service.ts
@@ -1,22 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Once } from 'necord';
 
-import { Client } from 'discord.js';
-
-import { CommandHandlerService } from './bot-handlers/command-handler.service';
-import { EventHandlerService } from './bot-handlers/event-handler.service';
-import { ButtonHandlerService } from './bot-handlers/button-handler.service';
-
 @Injectable()
 export class DiscordBotService {
-  constructor(
-    private readonly client: Client,
-    private readonly commandService: CommandHandlerService,
-    private readonly eventService: EventHandlerService,
-    private readonly buttonService: ButtonHandlerService,
-  ) {}
   private readonly logger = new Logger(DiscordBotService.name);
-  private readonly TIMEOUT = 1 * 60 * 1000;
 
   @Once('ready') // Когда бот запустился
   onReady() {
